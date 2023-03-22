09f7b942a7c7c898a3132021ea68b7efc6f4ff7a
Christian Hamann <krillee@lykoadmins-iMac.local>
2023-03-22 17:07:41 +0100

    Add func to turn off and morse for fun

--- uttag_test.py
+++ uttag_test.py
@@ -1,16 +1,38 @@
 from InquirerPy import inquirer
 import ShellyPy
+import time
 
-VPplug = ShellyPy.Shelly("<ipadress-to-plug>")
+VPplug = ShellyPy.Shelly("<ipadress-to-plug")
 
-while(True):
+while (True):
     lamp_on = inquirer.select(
-        message="Vill du trycka på lampan?:",
-        choices=["Ja", "Nej", "Ta mig härifrån!"],
+        message="Vad vill du göra?:",
+        choices=["Tända", "Släcka", "Morse", "Ta mig härifrån!"],
     ).execute()
 
-    if lamp_on == "Ja":
+    if lamp_on == "Tända":
         VPplug.relay(0, turn=True)
+    if lamp_on == "Släcka":
+        VPplug.relay(0, turn=False)
     if lamp_on == "Ta mig härifrån!":
         break
-        
+
+    if lamp_on == "Morse":
+        morse_kod = "-*.-.*.*...-*.-..*..*--./....*.*.-..*--." #Trevlig helg
+
+        morse = []
+        for char in morse_kod:
+            VPplug.relay(0, turn=False)
+            time.sleep(0.5)
+            if char == ".":
+                VPplug.relay(0, turn=True)
+                time.sleep(0.5)
+            elif char == "-":
+                VPplug.relay(0, turn=True)
+                time.sleep(1.5)
+            elif char == "*":
+                VPplug.relay(0, turn=False)
+                time.sleep(1)
+            elif char == "/":
+                VPplug.relay(0, turn=False)
+                time.sleep(3)
